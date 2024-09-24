54ff165da1367b42927f170a2a4d8fdd49b2baca
JeonDae <james911256@gmail.com>
2024-09-24 09:50:41 +0900

    함수

--- character_grass.py
+++ character_grass.py
@@ -1,12 +1,23 @@
 from pico2d import *
 import math
 
+def run_rectangle():
+    pass
+
+def run_circle():
+    pass
+
+
 open_canvas()
 
 #fill here
 
 grass = load_image('grass.png')
 character = load_image('character.png')
+
+while True:
+    run_rectangle()
+    run_circle()
 
 clear_canvas_now()
 
