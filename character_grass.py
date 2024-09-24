360ebe2ac826b5e673f421a3a50f2b0575a6b151
JeonDae <james911256@gmail.com>
2024-09-24 10:02:20 +0900

    캐릭터 출력

--- character_grass.py
+++ character_grass.py
@@ -1,11 +1,6 @@
 from pico2d import *
 import math
 
-def run_rectangle():
-    pass
-
-def run_circle():
-    pass
 
 
 open_canvas()
@@ -13,11 +8,25 @@ open_canvas()
 #fill here
 
 grass = load_image('grass.png')
-character = load_image('character.png')
+boy = load_image('character.png')
+
+def run_rectangle():
+    print('RECTANGLE')
+    pass
+
+def run_circle():
+    print('CIRCLE')
+    clear_canvas_now()
+    boy.draw_now(400,30)
+    delay(1)
+    
+    pass
+
 
 while True:
-    run_rectangle()
+    #run_rectangle()
     run_circle()
+    break
 
 clear_canvas_now()
 
